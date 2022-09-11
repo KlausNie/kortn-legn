5177c371611bc32c1397c9478725986cb56dc8ff
Klaus Niedermair <klaus.niedermair@quickbirdmedical.com>
2022-09-11 09:58:23 +0200

    chore: remove unused code

--- src/field/init_field.rs
+++ src/field/init_field.rs
@@ -3,14 +3,6 @@ use crate::Card;
 use crate::field::field::Field;
 
 pub fn init_field(cards: Vec<Card>) -> Field {
-    // Field {
-    //     top_stack1: cards.clone(),
-    //     top_stack2: cards.clone(),
-    //     bottom_stack1: cards.clone(),
-    //     bottom_stack2: cards.clone(),
-    //     bottom_stack3: cards.clone(),
-    //     not_played_cards: VecDeque::from(cards)
-    // }
     Field {
         top_stack1: vec![],
         top_stack2: vec![],
